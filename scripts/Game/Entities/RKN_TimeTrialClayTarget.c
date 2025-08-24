76878d1dc198649f4a239e24db4899c366744611
Rune Kofoed-Nissen <rune@beast.dk>
2025-08-24 22:12:05 +0200

    Import mission config. Add chat entity. Fix clay target not hiding on clients.

--- scripts/Game/Entities/RKN_TimeTrialClayTarget.c
+++ scripts/Game/Entities/RKN_TimeTrialClayTarget.c
@@ -12,14 +12,22 @@ class RKN_TimeTrialClayTarget : RKN_TimeTrialTargetEntity
 	
 	void Poof()
 	{
-		Show(false);
-		Update();
+		Rpc(RpcDo_Poof, false);
+		RpcDo_Poof(false);
 	}
 	
 	override void ResetTarget()
 	{
 		super.ResetTarget();
-		Show(true);
+		Rpc(RpcDo_Poof, true);
+		RpcDo_Poof(true);
+	}
+	
+	[RplRpc(RplChannel.Reliable, RplRcver.Broadcast)]
+	void RpcDo_Poof(bool unpoof)
+	{
+		Show(unpoof);
 		Update();
 	}
+	
 }
