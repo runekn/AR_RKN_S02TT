b255e7d1feb8617769a87b6276ffdaee75677c17
Rune Kofoed-Nissen <rune@beast.dk>
2025-08-24 17:36:56 +0200

    Fix voicelines in multiplayer

---
+++ scripts/Game/Action/RKN_ScenarioFrameworkActionBase.c
@@ -0,0 +1,12 @@
+modded class SCR_ScenarioFrameworkActionBase
+{
+	override bool ValidateInputEntity(IEntity object, SCR_ScenarioFrameworkGet getter, out IEntity entity)
+	{
+		if (!getter && object && !object.FindComponent(SCR_ScenarioFrameworkLayerBase))
+		{
+			entity = object;
+			return true;
+		}
+		return super.ValidateInputEntity(object, getter, entity);
+	}
+}
---
+++ scripts/Game/Entities/RKN_TImeTrialRadio.c
@@ -0,0 +1,9 @@
+class RKN_TimeTrialRadioClass : GenericEntityClass
+{
+}
+
+class RKN_TimeTrialRadio : GenericEntity
+{
+	[RplProp()]
+	int m_iDummy; // Random RplProp so Replication.FindId works
+}
